2b4ad1c5377ebcd120b016711ac4b52d609b1ab1
SwarupaPedapalli <77767471+SwarupaPedapalli@users.noreply.github.com>
2022-01-26 14:39:28 -0600

    Update 01mapper.py

--- 01mapper.py
+++ 01mapper.py
@@ -1,13 +1,13 @@
 # Swarupa Pedapalli
-# This is an example mapping
+# Pratice Mapper
 f = open("purchases.txt","r")  # open file, read-only
-o = open("Arla.txt", "w") # open file, write
+o = open("a..txt", "w") # open file, write
 for line in f:  
-    dataList = line.strip().split("    ") 
-    print (dataList )
-    print (len(dataList ))
-    if len(dataList) == 6:
-        date, time, location, dept, amount, payType = dataList  #assign names
+    rowList = line.strip().split("    ") 
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList  #assign names
         print ("{0}\t{1}".format(location, amount))
         o.write("{0}\t{1}\n".format(location, amount))
 f.close()
