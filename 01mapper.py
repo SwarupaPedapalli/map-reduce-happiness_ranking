82611a3ee4c414b550175ec5a9caf376fba35bba
SwarupaPedapalli <77767471+SwarupaPedapalli@users.noreply.github.com>
2022-01-28 19:44:43 -0600

    added reducer and sorter filesss

--- 01mapper.py
+++ 01mapper.py
@@ -1,14 +1,18 @@
 # Swarupa Pedapalli
 # Pratice Mapper
 f = open("purchase.txt","r")  # open file, read-only
-o = open("a..txt", "w") # open file, write
+o = open("a.txt", "w") # open file, write
+
 for line in f:  
     rowList = line.strip().split("    ") 
-    print (rowList )
-    print (len(rowList ))
+
+    # print (rowList )
+    # print (len(rowList ))
+
     if len(rowList) == 6:
         date, time, location, dept, amount, payType = rowList  #assign names
-        print ("{0}\t{1}".format(location, amount))
+        # print ("{0}\t{1}".format(location, amount))
         o.write("{0}\t{1}\n".format(location, amount))
+
 f.close()
 o.close()
